f8dca7e583e42880559a5919616ce42f4d8e3ca4
kkphxn <khyphan@ucdavis.edu>
2025-03-04 15:58:17 -0800

    52kmer

---
+++ 52kmercount.py
@@ -0,0 +1,24 @@
+import sys
+import gzip
+import mcb185
+import itertools
+
+
+k = int(sys.argv[2])
+kcount = {}
+for defline, seq in mcb185.read_fasta(sys.argv[1]):
+	for i in range(len(seq) -k +1):
+		kmer = seq[i:i+k]
+		if kmer not in kcount: kcount[kmer] = 0
+		kcount[kmer] += 1
+for kmer, n in kcount.items(): print(kmer, n)
+
+import itertools
+for nts  in intertools.product('ACGT', repeats=2):
+	print(nts)
+
+import itertools
+for nts in itertools.product('ACGT', repeat=k):
+	kmer = ''.join(nts)
+	if kmer in kcount: print(kmer, kcount[kmer])
+	else:			   print(kmer, 0)

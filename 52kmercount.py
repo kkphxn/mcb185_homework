c7c39634169b322f775429e6d5ed8db3fff686d0
kkphxn <khyphan@ucdavis.edu>
2025-03-13 14:38:51 -0700

    52kmer

--- 52kmercount.py
+++ 52kmercount.py
@@ -1,28 +1,24 @@
 import sys
-import gzip
 import mcb185
-import itertools 
+import itertools
 
 k = int(sys.argv[2])
 kcount = {}
-
 for defline, seq in mcb185.read_fasta(sys.argv[1]):
-	for i in range(len(seq) - k + 1):
-		kmer = seq[i:i+k]
-		if kmer not in kcount:
-			kcount[kmer] = 0
-		kcount[kmer] += 1
+	for i in range(len(seq) -k + 1):
+		kmer = seq[i:i+k] # windowing algorithm
+		if kmer not in kcount: kcount[kmer] = 0 # add into kcount
+		kcount[kmer] += 1 
+	for kmer, n in kcount.items():print(kmer,n) # n is count number
 
-for kmer, n in kcount.items():
-	print(kmer, n)
+# output: 16383   32766  194522
+# 16384 different sequences segment
+'''
+for nts in itertools.product('ACGT', repeat=2):
+    print(nts)
+#  all combinations of length 2 using the nucleotides'''
 
-# Fixing itertools spelling and using the correct 'repeat' keyword
-for nts in itertools.product('ACGT', repeat=2):  
-	print(nts)
-
-for nts in itertools.product('ACGT', repeat=k):
+for nts in itertools.product('ACGT',repeat=k):
 	kmer = ''.join(nts)
-	if kmer in kcount:
-		print(kmer, kcount[kmer])
-	else:
-		print(kmer, 0)
+	if kmer in kcount: print(kmer, kcount[kmer])
+	else:              print(kmer,0)
